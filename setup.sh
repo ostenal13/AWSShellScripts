308d7539bad53bd43e44d1ada0a85bf760ac1034
Alyssa Osten <ostenal@mounties.mansfield.edu>
2016-09-25 23:27:25 +0000

    AWS Web Server Setup Script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# by Alyssa Osten on 20160920
+# run as root; chmod 700 setup.sh; ./setup.sh 
+
+apt-get -qq update -y
+apt-get -qq upgrade -y
+
+hostnamectl set-hostname localhost
+hostnamectl
+
+apt-get -qq install -y zip unzip
+
+apt-get -qq install -y apache2
+systemctl restart apache2
+
+date
